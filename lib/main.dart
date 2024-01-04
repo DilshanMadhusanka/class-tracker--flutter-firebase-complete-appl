5ca41a1ac286b2fa4667552ce98f448fb199e849
DilshanMadhusanka <139458721+DilshanMadhusanka@users.noreply.github.com>
2024-01-04 23:41:09 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title:const  Text('Class Tracker'),
+      ),
+      body:const Center(
+        child:Text('Welcome to Class Tracker App!'),
+      ),
+    );
+  }
+}
